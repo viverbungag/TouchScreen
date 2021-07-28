04d9dabb5f4c04a1aada3fa2215c6e8b113263da
viverbungag <viverbungag1@gmail.com>
2021-07-28 14:44:09 +0800

    finished the ip address part

--- app/src/main/java/com/example/touchscreen/MainActivity.java
+++ app/src/main/java/com/example/touchscreen/MainActivity.java
@@ -2,23 +2,68 @@ package com.example.touchscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
+
 import java.net.*;
 import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    String IPadrress;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         View view = findViewById(R.id.screen);
+        final Context context = this;
 
+
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptsView = li.inflate(R.layout.prompts, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                context);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText userInput = (EditText) promptsView
+                .findViewById(R.id.editTextDialogUserInput);
+
+        // set dialog message
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                // get user input and set it to result
+                                // edit text
+                                IPadrress = userInput.getText().toString();
+
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
 
 
         View.OnTouchListener handleTouch = new View.OnTouchListener() {
@@ -45,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
                 try{
 
-                    new MyTask(coordinates).execute();
+                    new MyTask(coordinates, IPadrress).execute();
 
                 }catch(Exception ex){
                     System.out.println(ex);
@@ -66,9 +111,11 @@ public class MainActivity extends AppCompatActivity {
 
 class MyTask extends AsyncTask<Void, Void, Void> {
     String coordinates = "";
+    String IPadrress = "";
 
-    public MyTask(String coordinates){
+    public MyTask(String coordinates, String IPadrress){
         this.coordinates = coordinates;
+        this.IPadrress = IPadrress;
     }
 
     @Override
@@ -76,7 +123,7 @@ class MyTask extends AsyncTask<Void, Void, Void> {
 
         try {
             System.out.println(this.coordinates);
-            Socket s = new Socket("192.168.0.29", 8888);
+            Socket s = new Socket(IPadrress, 8888);
             PrintWriter pr = new PrintWriter(s.getOutputStream());
             pr.println(this.coordinates);
             pr.flush();
